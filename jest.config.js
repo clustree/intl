80da91e6e49f8141f49c80b6ef24dbd8ec0baf52
Loïc Padier <loic@clustree.com>
2018-03-14 14:09:12 +0100

    Update npmignore

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-
-module.exports = {
-  moduleNameMapper: {
-    "^@clustree/intl$": "<rootDir>/src/index.js",
-    "^@clustree/intl/macro$": "<rootDir>/macro/index.js"
-  },
-  verbose: true
-};
